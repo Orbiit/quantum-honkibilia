6acdabc29229a0cd6d65aca8ce82b533b5bbbd04
SheepTester <ussa.say@gmail.com>
2018-11-20 12:55:02 -0800

    more solid rectangles

--- ludantus.js
+++ ludantus.js
@@ -15,7 +15,7 @@ class Player {
     if (xv === 0 && yv === 0) {
       return {xv: xv, yv: yv, rect: null, side: null};
     }
-    let closestRectangle = null;
+    let closestHorizRect = null, closestVertRect = null;
     rectangles.forEach(rect => {
       if (rect.x < x + width + xv && x + xv < rect.x + rect.width
           && rect.y < y + height + yv && y + yv < rect.y + rect.height) {
@@ -32,6 +32,7 @@ class Player {
               changes.newyv = rect.y + width - y;
               changes.side = 'bottom';
             }
+            changes.horizontal = false;
             changes.squareDist = changes.newyv * changes.newyv;
           } else {
             const slope = yv / xv;
@@ -40,44 +41,55 @@ class Player {
             const anchory = yv > 0 ? y + height : y;
             const sidey = yv > 0 ? rect.y : rect.y + rect.height;
             let xCollided = false;
-            const collisionY = slope * (sidex - anchorx) + anchory;
-            if (collisionY >= rect.y && collisionY <= rect.y + rect.height) {
+            const collisionY = slope * (sidex - anchorx) + y;
+            if (collisionY <= rect.y + rect.height && rect.y <= collisionY + height) {
               xCollided = true;
               changes.newxv = sidex - anchorx;
-              changes.newyv = collisionY - anchory;
+              changes.newyv = collisionY - y;
               changes.squareDist = changes.newxv * changes.newxv + changes.newyv * changes.newyv;
               changes.side = xv > 0 ? 'left' : 'right';
+              changes.horizontal = true;
             }
-            const collisionX = (sidey - anchory) / slope + anchorx;
-            if (collisionX >= rect.x && collisionX <= rect.x + rect.width) {
-              const newxv = collisionX - anchorx, newyv = sidey - anchory;
+            const collisionX = (sidey - anchory) / slope + x;
+            if (collisionX <= rect.x + rect.width && rect.x <= collisionX + width) {
+              const newxv = collisionX - x, newyv = sidey - anchory;
               const squareDist = newxv * newxv + newyv * newyv;
-              if (xCollided) {
-                if (squareDist < changes.squareDist) {
-                  changes.newxv = newxv;
-                  changes.newyv = newyv;
-                  changes.squareDist = squareDist;
-                  changes.side = yv > 0 ? 'top' : 'bottom';
-                }
-              } else {
+              if (!xCollided || squareDist < changes.squareDist) {
                 changes.newxv = newxv;
                 changes.newyv = newyv;
                 changes.squareDist = squareDist;
                 changes.side = yv > 0 ? 'top' : 'bottom';
+                changes.horizontal = false;
               }
             }
           }
-          if (changes.side && (!closestRectangle || closestRectangle.squareDist > changes.squareDist))
-            closestRectangle = changes;
+          if (changes.side) {
+            if (changes.horizontal) {
+              if (!closestHorizRect || closestHorizRect.squareDist > changes.squareDist)
+                closestHorizRect = changes;
+            } else {
+              if (!closestVertRect || closestVertRect.squareDist > changes.squareDist)
+                closestVertRect = changes;
+            }
+          }
         }
       }
     });
-    if (closestRectangle) return {
-      xv: closestRectangle.newxv,
-      yv: closestRectangle.newyv,
-      rect: closestRectangle.rect,
-      side: closestRectangle.side
-    };
+    if (closestHorizRect || closestVertRect) {
+      let rect = closestHorizRect || closestVertRect;
+      if (closestHorizRect && closestVertRect) {
+        if (closestVertRect.squareDist < closestHorizRect.squareDist)
+          rect = closestVertRect;
+      }
+      const obj = {
+        xv: xv, yv: yv,
+        rect: rect.rect,
+        side: rect.side
+      };
+      if (closestHorizRect) obj.xv = closestHorizRect.newxv;
+      if (closestVertRect) obj.yv = closestVertRect.newyv;
+      return obj;
+    }
     else return {xv: xv, yv: yv, rect: null, side: null};
   }
 
